c99141c5cc7e7a0974ab8d1225a351480e62ebf8
droyd9 <100531900+droyd9@users.noreply.github.com>
2022-06-28 09:43:04 -0300

    Atualizar 'main.blocks, main.ts, main.py'

--- main.py
+++ main.py
@@ -1,3 +1,241 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+def on_button_pressed_a():
+    global lepra
+    lepra += 1
+    if lepra == 1:
+        basic.show_leds("""
+            # . . . .
+                        # # # . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
+    if lepra == 2:
+        basic.show_leds("""
+            # # # . .
+                        # . . . .
+                        # . . . .
+                        # . . . .
+                        . . . . .
+        """)
+    if lepra == 3:
+        basic.show_leds("""
+            # # # . .
+                        # . . . .
+                        # # # . .
+                        # . . . .
+                        . . . . .
+        """)
+    if lepra == 4:
+        basic.show_leds("""
+            # # # . .
+                        # . . . .
+                        # . . . .
+                        . . . . .
+                        . . . . .
+        """)
+    if lepra == 5:
+        basic.show_leds("""
+            # . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
+    if lepra == 6:
+        basic.show_leds("""
+            # . . . .
+                        # . . . .
+                        # # # . .
+                        # . . . .
+                        . . . . .
+        """)
+    if lepra == 7:
+        basic.show_leds("""
+            # # # . .
+                        # # # . .
+                        # . . . .
+                        . . . . .
+                        . . . . .
+        """)
+    if lepra == 7:
+        basic.show_leds("""
+            # # # . .
+                        # # # . .
+                        # . . . .
+                        . . . . .
+                        . . . . .
+        """)
+    if lepra == 8:
+        basic.show_leds("""
+            # . . . .
+                        # . . . .
+                        # . . . .
+                        # . . . .
+                        . . . . .
+        """)
+    if lepra == 9:
+        basic.show_leds("""
+            # . . . .
+                        # . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
+    if lepra == 10:
+        basic.show_leds("""
+            # . . . .
+                        # # # . .
+                        # # # . .
+                        # # # . .
+                        . . . . .
+        """)
+    if lepra == 11:
+        basic.show_leds("""
+            # # # . .
+                        # . . . .
+                        # # # . .
+                        . . . . .
+                        . . . . .
+        """)
+    if lepra == 12:
+        basic.show_leds("""
+            # . . . .
+                        # # # . .
+                        # . . . .
+                        # . . . .
+                        . . . . .
+        """)
+    if lepra == 13:
+        basic.show_leds("""
+            # # # . .
+                        # # # . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
+    if lepra == 14:
+        basic.show_leds("""
+            # # # . .
+                        # # # . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
+    if lepra == 15:
+        basic.show_leds("""
+            # # # . .
+                        # . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
+    if lepra == 1:
+        basic.show_leds("""
+            # # # . .
+                        # # # . .
+                        # # # . .
+                        . . . . .
+                        . . . . .
+        """)
+    if lepra == 16:
+        basic.show_leds("""
+            # . . . .
+                        # # # . .
+                        # # # . .
+                        # . . . .
+                        . . . . .
+        """)
+    if lepra == 17:
+        basic.show_leds("""
+            # # # . .
+                        # # # . .
+                        # . . . .
+                        # # # . .
+                        . . . . .
+        """)
+    if lepra == 18:
+        basic.show_leds("""
+            # . . . .
+                        # # # . .
+                        # . . . .
+                        . . . . .
+                        . . . . .
+        """)
+    if lepra == 19:
+        basic.show_leds("""
+            # . . . .
+                        # . . . .
+                        # . . . .
+                        . . . . .
+                        . . . . .
+        """)
+    if lepra == 20:
+        basic.show_leds("""
+            # # # . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+                        . . . . .
+        """)
+    if lepra == 21:
+        basic.show_leds("""
+            # . . . .
+                        # . . . .
+                        # # # . .
+                        . . . . .
+                        . . . . .
+        """)
+    if lepra == 22:
+        basic.show_leds("""
+            # . . . .
+                        # . . . .
+                        # . . . .
+                        # # # . .
+                        . . . . .
+        """)
+    if lepra == 23:
+        basic.show_leds("""
+            # . . . .
+                        # # # . .
+                        # # # . .
+                        . . . . .
+                        . . . . .
+        """)
+    if lepra == 24:
+        basic.show_leds("""
+            # # # . .
+                        # . . . .
+                        # . . . .
+                        # # # . .
+                        . . . . .
+        """)
+    if lepra == 25:
+        basic.show_leds("""
+            # # # . .
+                        # . . . .
+                        # # # . .
+                        # # # . .
+                        . . . . .
+        """)
+    if lepra == 26:
+        basic.show_leds("""
+            # # # . .
+                        # # # . .
+                        # . . . .
+                        # . . . .
+                        . . . . .
+        """)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    if lepra == 1:
+        basic.show_string("A")
+    elif lepra == 2:
+        basic.show_string("B")
+
+    else:
+        pass
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+lepra = 0
+lepra = 0
--- main.ts
+++ main.ts
@@ -126,7 +126,7 @@ input.onButtonPressed(Button.A, function () {
             . . . . .
             `)
     }
-    if (lepra == 13) {
+    if (lepra == 14) {
         basic.showLeds(`
             # # # . .
             # # # . .
@@ -135,7 +135,7 @@ input.onButtonPressed(Button.A, function () {
             . . . . .
             `)
     }
-    if (lepra == 14) {
+    if (lepra == 15) {
         basic.showLeds(`
             # # # . .
             # . . . .
@@ -144,7 +144,7 @@ input.onButtonPressed(Button.A, function () {
             . . . . .
             `)
     }
-    if (lepra == 15) {
+    if (lepra == 1) {
         basic.showLeds(`
             # # # . .
             # # # . .
@@ -198,39 +198,30 @@ input.onButtonPressed(Button.A, function () {
             . . . . .
             `)
     }
-    if (lepra == 20) {
-        basic.showLeds(`
-            # . . . .
-            # . . . .
-            # # # . .
-            . . . . .
-            . . . . .
-            `)
-    }
     if (lepra == 21) {
         basic.showLeds(`
             # . . . .
             # . . . .
-            # . . . .
             # # # . .
+            . . . . .
             . . . . .
             `)
     }
     if (lepra == 22) {
         basic.showLeds(`
             # . . . .
+            # . . . .
+            # . . . .
             # # # . .
-            # # # . .
-            . . . . .
             . . . . .
             `)
     }
     if (lepra == 23) {
         basic.showLeds(`
-            # # # . .
-            # . . . .
             # . . . .
             # # # . .
+            # # # . .
+            . . . . .
             . . . . .
             `)
     }
@@ -238,7 +229,7 @@ input.onButtonPressed(Button.A, function () {
         basic.showLeds(`
             # # # . .
             # . . . .
-            # # # . .
+            # . . . .
             # # # . .
             . . . . .
             `)
@@ -246,11 +237,77 @@ input.onButtonPressed(Button.A, function () {
     if (lepra == 25) {
         basic.showLeds(`
             # # # . .
+            # . . . .
+            # # # . .
+            # # # . .
+            . . . . .
+            `)
+    }
+    if (lepra == 26) {
+        basic.showLeds(`
+            # # # . .
             # # # . .
             # . . . .
             # . . . .
             . . . . .
             `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (lepra == 1) {
+        basic.showString("A")
+    } else if (lepra == 2) {
+        basic.showString("B")
+    } else if (lepra == 3) {
+        basic.showString("C")
+    } else if (lepra == 4) {
+        basic.showString("D")
+    } else if (lepra == 5) {
+        basic.showString("E")
+    } else if (lepra == 6) {
+        basic.showString("F")
+    } else if (lepra == 7) {
+        basic.showString("G")
+    } else if (lepra == 8) {
+        basic.showString("H")
+    } else if (lepra == 9) {
+        basic.showString("I")
+    } else if (lepra == 10) {
+        basic.showString("J")
+    } else if (lepra == 11) {
+        basic.showString("K")
+    } else if (lepra == 12) {
+        basic.showString("L")
+    } else if (lepra == 13) {
+        basic.showString("M")
+    } else if (lepra == 14) {
+        basic.showString("N")
+    } else if (lepra == 15) {
+        basic.showString("O")
+    } else if (lepra == 16) {
+        basic.showString("P")
+    } else if (lepra == 17) {
+        basic.showString("Q")
+    } else if (lepra == 18) {
+        basic.showString("R")
+    } else if (lepra == 19) {
+        basic.showString("S")
+    } else if (lepra == 20) {
+        basic.showString("T")
+    } else if (lepra == 21) {
+        basic.showString("U")
+    } else if (lepra == 22) {
+        basic.showString("V")
+    } else if (lepra == 23) {
+        basic.showString("W")
+    } else if (lepra == 24) {
+        basic.showString("X")
+    } else if (lepra == 25) {
+        basic.showString("Y")
+    } else if (lepra == 26) {
+        basic.showString("Z")
+    } else {
+        lepra = 0
     }
 })
 let lepra = 0
