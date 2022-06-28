0fc754d3896e69678305512d6fc77cffe7177f34
droyd9 <100531900+droyd9@users.noreply.github.com>
2022-06-28 09:17:21 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,257 @@
+input.onButtonPressed(Button.A, function () {
+    lepra += 1
+    if (lepra == 1) {
+        basic.showLeds(`
+            # . . . .
+            # # # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 2) {
+        basic.showLeds(`
+            # # # . .
+            # . . . .
+            # . . . .
+            # . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 3) {
+        basic.showLeds(`
+            # # # . .
+            # . . . .
+            # # # . .
+            # . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 4) {
+        basic.showLeds(`
+            # # # . .
+            # . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 5) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 6) {
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # # # . .
+            # . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 7) {
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 7) {
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 8) {
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 9) {
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 10) {
+        basic.showLeds(`
+            # . . . .
+            # # # . .
+            # # # . .
+            # # # . .
+            . . . . .
+            `)
+    }
+    if (lepra == 11) {
+        basic.showLeds(`
+            # # # . .
+            # . . . .
+            # # # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 12) {
+        basic.showLeds(`
+            # . . . .
+            # # # . .
+            # . . . .
+            # . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 13) {
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 13) {
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 14) {
+        basic.showLeds(`
+            # # # . .
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 15) {
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            # # # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 16) {
+        basic.showLeds(`
+            # . . . .
+            # # # . .
+            # # # . .
+            # . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 17) {
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            # . . . .
+            # # # . .
+            . . . . .
+            `)
+    }
+    if (lepra == 18) {
+        basic.showLeds(`
+            # . . . .
+            # # # . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 19) {
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 20) {
+        basic.showLeds(`
+            # # # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 20) {
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # # # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 21) {
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # . . . .
+            # # # . .
+            . . . . .
+            `)
+    }
+    if (lepra == 22) {
+        basic.showLeds(`
+            # . . . .
+            # # # . .
+            # # # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (lepra == 23) {
+        basic.showLeds(`
+            # # # . .
+            # . . . .
+            # . . . .
+            # # # . .
+            . . . . .
+            `)
+    }
+    if (lepra == 24) {
+        basic.showLeds(`
+            # # # . .
+            # . . . .
+            # # # . .
+            # # # . .
+            . . . . .
+            `)
+    }
+    if (lepra == 25) {
+        basic.showLeds(`
+            # # # . .
+            # # # . .
+            # . . . .
+            # . . . .
+            . . . . .
+            `)
+    }
+})
+let lepra = 0
+lepra = 0
